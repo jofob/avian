7a25604b944cd7fbdc39e38fad39afb0b8ffa727
Julia O'Flynn O'Brien <oflynnoj@tcd.ie>
2015-07-28 15:53:27 +0100

    Stores paths and points coordinates

---
+++ pathsAndPoints.js
@@ -0,0 +1,32 @@
+//This file contains the hard coded paths and points for doves and crows
+
+//JSON
+
+//Paths - will be three dimensional arrays of lon,lat to create poly lines
+var dovePaths = {
+				"coords" : 
+					[[[53.3390956,-6.2774888],
+					[53.345664,-6.2690294], 
+					[53.353964,-6.2525499]],
+				
+					[[53.3390956,-6.2774888],
+					[53.3489432,-6.2793291], 
+					[53.3592403,-6.2934482]]]
+				};
+				
+				
+				
+var crowPaths = {
+				"coords" : 
+					[[[53.3390956,-6.2774888],
+					[53.345664,-6.2690294], 
+					[53.353964,-6.2525499]],
+				
+					[[53.3390956,-6.2774888],
+					[53.3489432,-6.2793291], 
+					[53.3592403,-6.2934482]]]
+				};
+
+var dovePoints = [];
+var crowPoints = [];
+
